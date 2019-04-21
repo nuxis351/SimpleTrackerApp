58d7f53f771c61a38fa2725b77e8b3e90956e9ca
Illya Yefimenko <iyefimenko@bearincorp.com>
2019-04-21 15:58:53 -0400

    created nav drawer

--- app/src/main/java/nuxis351/github/com/simpletracker/MainActivity.java
+++ app/src/main/java/nuxis351/github/com/simpletracker/MainActivity.java
@@ -2,12 +2,20 @@ package nuxis351.github.com.simpletracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
     }
 }
