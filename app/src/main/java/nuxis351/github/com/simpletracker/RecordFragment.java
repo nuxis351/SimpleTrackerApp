e4d12e29de4a7c35106945ac61463375a0be4c7e
Illya Yefimenko <iyefimenko@bearincorp.com>
2019-04-24 14:14:46 -0400

    added chrono functionality

--- app/src/main/java/nuxis351/github/com/simpletracker/RecordFragment.java
+++ app/src/main/java/nuxis351/github/com/simpletracker/RecordFragment.java
@@ -5,14 +5,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 /**
  * Created by nuxis on 4/23/2019.
  */
 
 public class RecordFragment extends Fragment {
+    Chronometer chrono;
+    Button startTrackerButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.record_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        initializeViews(view);
+
+        startTrackerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chrono.start();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        chrono.stop();
+    }
+
+    private void initializeViews(View view){
+        chrono = view.findViewById(R.id.trip_chrono);
+        startTrackerButton = view.findViewById(R.id.start_stop_tracker_button);
     }
 }
