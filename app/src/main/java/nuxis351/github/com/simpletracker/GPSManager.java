49c7ed7761a2b7635c68cbc08c2269d845ad3bc2
Illya Yefimenko <iyefimenko@bearincorp.com>
2019-07-15 13:09:10 -0400

    Revert "fixed LocationService distance polling"
    This reverts commit f373d6b3aa328bca3b11375506b8706a0dacb7ed.

--- app/src/main/java/nuxis351/github/com/simpletracker/GPSManager.java
+++ app/src/main/java/nuxis351/github/com/simpletracker/GPSManager.java
@@ -3,7 +3,6 @@ package nuxis351.github.com.simpletracker;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.SystemClock;
 
 /**
  * Created by nuxis on 5/1/2019.
@@ -17,15 +16,13 @@ public class GPSManager {
 
     private boolean locationSkipped;
 
-    static private long CHRONO_BASE;
     final private double MIN_LOCATION_ACCURACY = 10;
 
-    public GPSManager(long CHRONO_BASE){
-        this.CHRONO_BASE = CHRONO_BASE;
-        this.previousLocation = new Location("dummyprovidor");
-        this.distance = 0;
-        this.topSpeed = 0;
-        this.avgSpeed = 0;
+    public GPSManager(){
+        previousLocation = new Location("dummyprovidor");
+        distance = 0;
+        topSpeed = 0;
+        avgSpeed = 0;
     }
 
     public void tick(Location currentLocation){
@@ -33,49 +30,24 @@ public class GPSManager {
             if(verifyLocationAccuracy(currentLocation)){
                 if(locationSkipped){
                     locationSkipped = false;
+                    previousLocation.set(currentLocation);
                 } else {
-                    setDistance(calculateDistance(previousLocation, currentLocation, distance));
+                    distance += previousLocation.distanceTo(currentLocation);
+                    previousLocation.set(currentLocation);
                 }
-                setTopSpeed(calculateTopSpeed(topSpeed, currentLocation));
-                calculateAvgSpeed(distance, getElapsedTimeMillis());
-                previousLocation.set(currentLocation);
-
+                if(currentLocation.hasSpeed()){
+                    if(currentLocation.getSpeed() > topSpeed){
+                        topSpeed = currentLocation.getSpeed();
+                    }
+                }
             } else {
                 locationSkipped = true;
             }
         }
     }
 
-    private double calculateDistance(Location previousLocation, Location currentLocation, double totalDistance){
-        return totalDistance + previousLocation.distanceTo(currentLocation);
-    }
-    private double calculateTopSpeed(double previousTopSpeed, Location currentLocation){
-        if(currentLocation.hasSpeed()){
-            //convert speed from meters/second here
-            if(currentLocation.getSpeed() > previousTopSpeed){
-                return currentLocation.getSpeed();
-            }
-        }
-        return previousTopSpeed;
-    }
-    private double calculateAvgSpeed(double distance, long ElapsedTime){ //assumes distance as miles/kilometers
-        return distance / ((double) ((ElapsedTime / (1000*60*60)) % 24)); // convert millis elapsed time to hours. divide distance by hours.
-    }
-    private long getElapsedTimeMillis(){
-        return SystemClock.elapsedRealtime() - CHRONO_BASE;
-    }
-
     private boolean verifyLocationAccuracy(Location location){
         return location.getAccuracy() < MIN_LOCATION_ACCURACY;
-    }
-
-    private void setTopSpeed(double topSpeed){
-        if(this.topSpeed < topSpeed){
-            this.topSpeed = topSpeed;
-        }
-    }
-    private void setDistance(double distance){
-        this.distance = distance; // code to change meters to feet / miles / kilometer goes here?
     }
 
     public double getDistance(){
